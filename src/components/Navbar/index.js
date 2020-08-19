15d09ff5369fe4f130c891397e5dc3c975457a0e
Victor <zedfromionia@gmail.com>
2020-08-18 22:07:42 -0300

    add svg assets

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
+import logo from '../../logo.svg';
+import home from '../../assets/home-outline.svg';
+import compass from '../../assets/compass-outline.svg';
 import Nav from './styled';
 
 const Navbar = () => (
     <Nav>
-        <ul>
-            <li>Some</li>
-            <li>Other</li>
-        </ul>
+        <div>
+            <img src={logo} alt="logo" height={30} />
+            <input type="text" />
+            <ul>
+                <li>
+                    <img src={home} alt="home" height={22} width={22} />
+                </li>
+                <li>
+                    <img src={compass} alt="compass" height={22} width={22} />
+                </li>
+            </ul>
+        </div>
     </Nav>
 );
 
--- src/components/Navbar/styled.js
+++ src/components/Navbar/styled.js
@@ -2,16 +2,38 @@ import styled from 'styled-components';
 
 const Nav = styled.nav`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     height: 3.375rem;
     width: 100vw;
     background-color: ${({ theme }) => theme.colors.bg};
     position: fixed;
+    border-bottom: 0.5px solid lightgrey;
+
+    div {
+        width: 100%;
+        max-width: 975px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    img {
+        margin-top: 8px;
+    }
+
+    input {
+        height: 28px;
+    }
 
     ul {
         list-style: none;
         display: flex;
         flex-direction: row;
+        justify-content: center;
+        padding: 0 2rem;
     }
 
     @media screen and (max-width: 44rem) {
